d59a18bfec0e69e79155e5b03cc3f7643b75486c
AMS\ddizmang <ddizmang@scramsystems.com>
2021-07-10 10:40:30 -0600

    Add project files.

---
+++ Joystick_UI_Azure_SpecFlow_MSTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Joystick_UI_Azure_SpecFlow_MSTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
